367850f991eeafda158aa0de44586091c21b8ce4
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-01-24 15:54:58 -0500

    almost done with lots of towers

--- generate.py
+++ generate.py
@@ -7,16 +7,31 @@ width=1
 height=1
 x=0
 y=0
-z=0
+z=0.5
 #box 2 variables
-length1=1
-width1=1
-height1=1
+#length1=1
+#width1=1
+#height1=1
 
-x1=1
-y1=1
-z1=1
+#x1=1
+#y1=1
+#z1=1
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length1,width1,height1])
+#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length1,width1,height1])
+for k in range(10):
+    for j in range(6):
+        for i in range(6):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            x=i
+            y=j
+            z=k+0.5
+            length = length*0.9
+            width=width*0.9
+            height=height*0.9
+            
+            
+    
 pyrosim.End()
+
+            
--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("boxes.sdf")
-for i in range(1000):
+for i in range(10000):
     sleep(1/60)
     p.stepSimulation()
     print(i)
