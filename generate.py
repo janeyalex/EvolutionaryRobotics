713d121c2078941e347d867e24544bf5dfcf738e
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-01-24 16:06:20 -0500

    almost fixed it

--- generate.py
+++ generate.py
@@ -20,15 +20,17 @@ z=0.5
 #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 #pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length1,width1,height1])
 for k in range(10):
+    length = length*0.9
+    width=width*0.9
+    height=height*0.9
     for j in range(6):
         for i in range(6):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-            x=i
-            y=j
-            z=k+0.5
-            length = length*0.9
-            width=width*0.9
-            height=height*0.9
+            pyrosim.Send_Cube(name="Box", pos=[i,j,k+0.5] , size=[length,width,height])
+            #x=i
+            #y=j
+            #z=k+0.5
+
+                
             
             
     
