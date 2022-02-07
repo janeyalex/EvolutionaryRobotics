8e1f1850499b5d2e68c09c51cac8ea86695b8df9
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-02-07 15:21:03 -0500

    fixed issue

--- generate.py
+++ generate.py
@@ -33,10 +33,10 @@ def Create_Robot():
     # pyrosim.Send_Cube(name="Link_6", pos=[0,0,-0.5] , size=[length,width,height])
     
     pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[length,width,height])
-    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1])
-    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,-0.5] , size=[length,width,height])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,-0.5] , size=[length,width,height]) 
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length,width,height]) 
 
  
     pyrosim.End()
