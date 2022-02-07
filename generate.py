db0b735de53abbc7ba03ec2e346b5d945f179912
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-02-07 15:10:54 -0500

    hw4

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def Create_World():
     x=1
     y=2
     z=0.5
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])       
+   # pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])       
     pyrosim.End()
 
 def Create_Robot():
@@ -35,10 +35,8 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[length,width,height])
     pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1])
     pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1])
-
     pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,-0.5] , size=[length,width,height])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,-0.5] , size=[length,width,height]) 
-
 
  
     pyrosim.End()
