0514c7f2002883c240ed949c237d06598bbd8f0b
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-02-14 15:09:28 -0500

    step 77

--- sensor.py
+++ sensor.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pyrosim.pyrosim as pyrosim
+
 class SENSOR:
     def __init__(self, linkName):
         self.linkName= linkName
+        self.values = np.zeros(1000)
+        
+    def getValue(self,timeStep):
+        self.timeStep =timeStep
+        self.values[self.timeStep] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+        if (self.timeStep==999):
+            print(self.values)
