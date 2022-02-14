2d27a14131798d31250cc54c6e7d385306d327b2
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-02-14 09:18:33 -0500

    starting refactoring

---
+++ constants.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#back leg variables
+amplitudeBack = np.pi/6
+frequencyBack = 10
+phaseOffsetBack = np.pi/4
+
+#front leg variables
+amplitudeFront = np.pi/6
+frequencyFront = 10
+phaseOffsetFront = -(3/2)*np.pi
+
+maxForce = 200
--- simulate.py
+++ simulate.py
@@ -1,85 +1,87 @@
-from time import sleep
-import pybullet as p 
-import pybullet_data
-import pyrosim.pyrosim as pyrosim
-import numpy as np
-import os
-import math
-import random
+# from time import sleep
+# import pybullet as p 
+# import pybullet_data
+# import pyrosim.pyrosim as pyrosim
+# import numpy as np
+# import os
+# import math
+# import random
+# import constants as c
 
-#back leg variables
-amplitudeBack = np.pi/6
-frequencyBack = 10
-phaseOffsetBack = np.pi/4
+# #back leg variables
+# amplitudeBack = c.amplitudeBack
+# frequencyBack = c.frequencyBack
+# phaseOffsetBack = c.phaseOffsetBack
 
-#front leg variables
-amplitudeFront = np.pi/6
-frequencyFront = 10
-phaseOffsetFront = -(3/2)*np.pi
-#amplitude * sin(frequency * i + phaseOffset)
+# #front leg variables
+# amplitudeFront = c.amplitudeFront
+# frequencyFront = c.frequencyFront
+# phaseOffsetFront = c.phaseOffsetFront
+# #amplitude * sin(frequency * i + phaseOffset)
 
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
-p.loadSDF("world.sdf")
+# physicsClient = p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# p.setGravity(0,0,-9.8)
+# planeId = p.loadURDF("plane.urdf")
+# robotId = p.loadURDF("body.urdf")
+# p.loadSDF("world.sdf")
 
-pyrosim.Prepare_To_Simulate(robotId)
+# pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = np.zeros(1000)
-frontLegSensorValues = np.zeros(1000)
+# backLegSensorValues = np.zeros(1000)
+# frontLegSensorValues = np.zeros(1000)
 
-x = np.linspace(0, 2*(np.pi), 1000)
+# x = np.linspace(0, 2*(np.pi), 1000)
 
-targetAnglesBack = np.zeros(1000)
-targetAnglesFront = np.zeros(1000)
-for i in range(1000):
-    targetAnglesBack[i] = amplitudeBack *(np.sin((frequencyBack*x[i])+phaseOffsetBack))
-    targetAnglesFront[i] = amplitudeFront *(np.sin((frequencyFront*x[i])+phaseOffsetFront))
+# targetAnglesBack = np.zeros(1000)
+# targetAnglesFront = np.zeros(1000)
+# for i in range(1000):
+#     targetAnglesBack[i] = amplitudeBack *(np.sin((frequencyBack*x[i])+phaseOffsetBack))
+#     targetAnglesFront[i] = amplitudeFront *(np.sin((frequencyFront*x[i])+phaseOffsetFront))
 
 
+
+# # save_path = '/Users/janeyalex/Documents/CS206/JaneysBots/data'
+# # file_name2 = "sinBackData"
+# # completeName2 = os.path.join(save_path, file_name2)
+# # np.save(completeName2,targetAnglesBack)
+
+# # file_name3 = "sinFrontData"
+# # completeName3 = os.path.join(save_path, file_name3)
+# # np.save(completeName3,targetAnglesFront)
+
+# # exit()
+
+# for i in range(1000):
+#     sleep(1/1000)
+#     p.stepSimulation()
+#     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+#     frontLegSensorValues[i]= pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+#     pyrosim.Set_Motor_For_Joint(
+#         bodyIndex = robotId,
+#         jointName = "Torso_BackLeg",
+#         controlMode = p.POSITION_CONTROL,
+#         targetPosition = targetAnglesBack[i],
+#         maxForce = c.maxForce)
+    
+#     pyrosim.Set_Motor_For_Joint(
+#         bodyIndex = robotId,
+#         jointName = "Torso_FrontLeg",
+#         controlMode = p.POSITION_CONTROL,
+#         targetPosition = targetAnglesFront[i],
+#         maxForce = c.maxForce)
+
+# p.disconnect()
 
 # save_path = '/Users/janeyalex/Documents/CS206/JaneysBots/data'
-# file_name2 = "sinBackData"
-# completeName2 = os.path.join(save_path, file_name2)
-# np.save(completeName2,targetAnglesBack)
+# file_name = "sensoryDataBackLeg"
+# completeName = os.path.join(save_path, file_name)
 
-# file_name3 = "sinFrontData"
-# completeName3 = os.path.join(save_path, file_name3)
-# np.save(completeName3,targetAnglesFront)
+# np.save(completeName,backLegSensorValues)
 
-# exit()
+# file_name1 = "sensoryDataFrontLeg"
+# completeName1 = os.path.join(save_path, file_name1)
 
-for i in range(1000):
-    sleep(1/1000)
-    p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i]= pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex = robotId,
-        jointName = "Torso_BackLeg",
-        controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAnglesBack[i],
-        maxForce = 200)
-    
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex = robotId,
-        jointName = "Torso_FrontLeg",
-        controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAnglesFront[i],
-        maxForce = 200)
+# np.save(completeName1,frontLegSensorValues)
 
-p.disconnect()
-
-save_path = '/Users/janeyalex/Documents/CS206/JaneysBots/data'
-file_name = "sensoryDataBackLeg"
-completeName = os.path.join(save_path, file_name)
-
-np.save(completeName,backLegSensorValues)
-
-file_name1 = "sensoryDataFrontLeg"
-completeName1 = os.path.join(save_path, file_name1)
-
-np.save(completeName1,frontLegSensorValues)
-
+pass
