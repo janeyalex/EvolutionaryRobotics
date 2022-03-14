39227afdd0ee3adfa5e8cda577d34fd733ecac8f
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-03-14 17:57:36 -0400

    added parallelHillClimber.py

---
+++ parallelHillClimber.py
@@ -0,0 +1,43 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+class HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()
+
+    def Evolve(self):
+        self.parent.Evaluate("GUI")
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+
+    def Mutate(self):
+        self.child.Mutate()
+        # print(self.parent.weights)
+        # print(self.child.weights)
+
+    def Select(self):
+        if (self.parent.fitness > self.child.fitness):
+            self.parent = self.child
+
+
+    def Print(self):
+        print("")
+        print("")
+        print("Parent Fitness: ", self.parent.fitness)
+        print("Child Fitness: ", self.child.fitness)
+        print("")
+
+    def Show_Best(self):
+        self.parent.Evaluate('GUI')
