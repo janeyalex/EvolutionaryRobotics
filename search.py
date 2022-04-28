057f2ae6a3712339ab90897b74268ac8d51c049f
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-04-27 21:53:18 -0400

    ran A

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 #     os.system("python3 generate.py")
 #     os.system("python3 simulate.py")
 dataID= 1
-for i in range(2):
+for i in range(20):
     phc = PARALLEL_HILL_CLIMBER(dataID)
     phc.Evolve()
     phc.Show_Best()
