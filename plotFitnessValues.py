bb21b2abd0b07674ffe4969d2739fe042fa0d291
janeyalex <60708261+janeyalex@users.noreply.github.com>
2022-05-02 12:41:16 -0400

    graphed all simulations

--- plotFitnessValues.py
+++ plotFitnessValues.py
@@ -3,38 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import constants as c
 
+avgAdata = np.empty([20,50])
+avgBdata = np.empty([20,50])
 
-fitA = np.load('/Users/janeyalex/Documents/CS206/JaneysBots/data/fitnessDataA.npy')
-fitB = np.load('/Users/janeyalex/Documents/CS206/JaneysBots/data/fitnessDataB.npy')
+for i in range(1,21):
+    filename = '/Users/janeyalex/Documents/CS206/JaneysBots/data/fitnessDataA'
+    filename = filename + str(i)+'.npy'
+    fitA = np.load(filename)
+    fitAvgA = np.average(fitA,axis=0)
+    avgAdata[i-1] = fitAvgA
+
+    filename1 = '/Users/janeyalex/Documents/CS206/JaneysBots/data/fitnessDataB'
+    filename1 = filename1 + str(i)+'.npy'
+    fitB = np.load(filename1)
+    fitAvgB = np.average(fitB,axis=0)
+    avgBdata[i-1] = fitAvgB
 
 
+overallAvgA = np.average(avgAdata,axis=0)
+overallAvgB = np.average(avgBdata,axis=0)
 
-xNum = [1,2,3,4,5,6,7,8,9,10]
+stdDevA = np.std(avgAdata, axis = 0)
+stdDevB = np.std(avgBdata, axis = 0)
 
 
-# for i in range(c.numberOfGenerations):
-#     plt.plot(xNum,fitA[i,:],'r-',linewidth = 3)
-    
-#     plt.plot(xNum,fitB[i,:],'b*-')
-    
-# plt.plot([],[], 'r-', label='Robot A')
-# plt.plot([],[], 'b*-', label='Robot B')
+xNum = list(np.arange(1,51))
 
-# plt.xlim(1, 10)
-# plt.xlabel('Run in the Popluation')
-# plt.ylabel('Fitness Value')
-# plt.title('Fitness of A and B Robot (Population Size= 10, Generations = 10)' )
-# plt.legend()
-# plt.show()
-
-fitAvgA = np.average(fitA,axis=0)
-fitAvgB = np.average(fitB,axis=0)
-
-plt.plot(xNum,fitAvgA,'r-', label = 'Robot A')
-plt.plot(xNum,fitAvgB,'b*-', label = 'Robot B')
-plt.xlim(1, 10)
+plt.plot(xNum,overallAvgA,'r-', label = 'Robot A')
+plt.plot(xNum,overallAvgB,'b*-', label = 'Robot B')
+# plt.errorbar(xNum, overallAvgA, yerr = stdDevA,fmt='o',ecolor = 'cyan')
+plt.errorbar(xNum, overallAvgB, yerr = stdDevB,fmt='o',ecolor = 'magenta')
+plt.xlim(1, 50)
 plt.xlabel('Generation')
-plt.ylabel('Averaged Fitness Value over 10 Runs')
-plt.title('Fitness of A and B Robot (Population Size= 10, Generations = 10)' )
+plt.ylabel('Averaged Fitness Value')
+plt.title('Fitness of A and B Robot 20 simulations (Population Size= 10, Generations = 50)' )
 plt.legend()
 plt.show()
+
